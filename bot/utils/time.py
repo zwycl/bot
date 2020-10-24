862fb070a501ca45cccb481d62f079b3bdb1d16f
Ziwei Ye <zxy1677@rit.edu>
2020-10-24 14:13:11 -0400

    init
    init

--- bot/utils/time.py
+++ bot/utils/time.py
@@ -94,7 +94,6 @@ def parse_rfc1123(stamp: str) -> datetime.datetime:
     return datetime.datetime.strptime(stamp, RFC1123_FORMAT).replace(tzinfo=datetime.timezone.utc)
 
 
-# Hey, this could actually be used in the off_topic_names and reddit cogs :)
 async def wait_until(time: datetime.datetime, start: Optional[datetime.datetime] = None) -> None:
     """
     Wait until a given time.
